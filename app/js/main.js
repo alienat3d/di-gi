38b89a7c49c8ac78465c5d68e26cbc5739139973
Al Tsaplin <al.tsaplin@gmail.com>
2021-07-08 16:16:34 +0300

    head sect finished

--- app/js/main.js
+++ app/js/main.js
@@ -3,19 +3,21 @@ $(function(){
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    prevArrow:
+      '<button class="slick-arrow slick-prev" type="button"><img class="top-slider__button-img-left" src="images/icons/slider-arrow-left.svg" alt="slider left arrow button"></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next" type="button"><img class="top-slider__button-img-right" src="images/icons/slider-arrow-right.svg" alt="slider right arrow button"></button>',
     responsive: [
       {
         breakpoint: 1024,
-        settings: {
-
-        }
+        settings: {},
       },
       {
         breakpoint: 640,
         settings: {
-          arrows: false
-        }
-      }
-    ]
+          arrows: false,
+        },
+      },
+    ],
   });
 });
