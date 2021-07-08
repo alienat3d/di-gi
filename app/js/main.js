3aa4ff7d98bffadab74ca57fe43132427af5036a
Al Tsaplin <al.tsaplin@gmail.com>
2021-07-08 18:02:04 +0300

    finished call-us sect

--- app/js/main.js
+++ app/js/main.js
@@ -9,15 +9,11 @@ $(function(){
       '<button class="slick-arrow slick-next" type="button"><img class="top-slider__button-img-right" src="images/icons/slider-arrow-right.svg" alt="slider right arrow button"></button>',
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {},
-      },
-      {
         breakpoint: 640,
         settings: {
           arrows: false,
-        },
-      },
-    ],
+        }
+      }
+    ]
   });
 });
