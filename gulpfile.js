227765b89948bccb4ff35bc666b5b083033b2f1e
Al Tsaplin <al.tsaplin@gmail.com>
2022-11-22 19:49:41 +0300

    latest

--- gulpfile.js
+++ gulpfile.js
@@ -45,8 +45,7 @@ function scripts() {
 
 function images() {
   return src("app/images/**/*")
-    .pipe(imagemin(
-      [
+    .pipe(imagemin([
         imagemin.gifsicle({ interlaced: true }),
         imagemin.mozjpeg({ quality: 75, progressive: true }),
         imagemin.optipng({ optimizationLevel: 5 }),
